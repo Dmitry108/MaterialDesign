0d8e6c036e1ba6944d5620dd9dd9384c862f8e5f
Dmitrii Babanov <babanov1984@mail.ru>
2020-07-30 03:21:02 +0300

    homework3

---
+++ lessons/src/main/java/ru/bdim/lessons/TextActivity.kt
@@ -0,0 +1,12 @@
+package ru.bdim.lessons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TextActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_textinputlayout)
+    }
+}
