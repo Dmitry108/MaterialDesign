0be3059f09c7e8c926182f88e30ea669b8557bd5
Dmitry Babanov <babnov1984@mail.ru>
2020-08-20 19:12:05 +0300

    homework 8

---
+++ lessons/src/main/java/ru/bdim/lessons/StyleActivity.kt
@@ -0,0 +1,11 @@
+package ru.bdim.lessons
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StyleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_style)
+    }
+}
